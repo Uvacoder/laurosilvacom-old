47bbb5c0b6349e64131ab03ee11b628f1ff1ca3d
Lauro Silva <hey@laurosilva.com>
2020-04-01 16:50:51 -0700

    Adding about image and content

--- src/components/image.js
+++ src/components/image.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import {useStaticQuery, graphql} from 'gatsby'
+import styled from 'styled-components'
 import Img from 'gatsby-image'
 
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      aboutImage: file(relativePath: {eq: "lauro.jpg"}) {
+      aboutImage: file(relativePath: {eq: "lauro.png"}) {
         childImageSharp {
-          fluid(maxWidth: 1200) {
-            ...GatsbyImageSharpFluid_withWebp
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
@@ -16,12 +17,26 @@ const Image = () => {
   `)
 
   return (
-    <Img
-      fadeIn="true"
-      loading="lazy"
-      fluid={data.aboutImage.childImageSharp.fluid}
-    />
+    <ImageWrapper>
+      <Img fluid={data.aboutImage.childImageSharp.fluid} />
+    </ImageWrapper>
   )
 }
+
+const ImageWrapper = styled.div`
+  margin-top: -250px;
+  margin-bottom: 60px;
+  border-radius: 10px;
+  box-shadow: rgba(198, 208, 235, 0.5) 0px 30px 60px;
+  .gatsby-image-wrapper {
+    border-radius: 10px;
+  }
+  .dark & {
+    box-shadow: rgba(0, 0, 0, 0.5) 0px 30px 60px;
+  }
+  @media (max-width: ${props => props.theme.screen.sm}) {
+    margin-top: -150px;
+  }
+`
 
 export default Image
